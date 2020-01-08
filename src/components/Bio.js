8d545b211dc0ff33ce3b29dad2339313bde77cb3
Mostafa <gmelkabany@gmail.com>
2020-01-08 21:19:38 +0200

    3rd post

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -18,9 +18,9 @@ function Bio(props) {
         Useful Tutorials in Useful topics.{' '}
         <br />
         Hire me!{' '}
-        <a href="https://twitter.com/mostafakab">Moustafa Elkabany</a>.
+        <a href="https://www.upwork.com/freelancers/~013c25dd09c0c6d200">Moustafa Elkabany</a>.
         <br />
-        Follow me on <a href="https://www.upwork.com/freelancers/~013c25dd09c0c6d200">twitter.</a>.
+        Follow me on <a href="https://twitter.com/mostafakab">twitter.</a>.
         {/* <a href="https://overreacted.io/">overreacted.io</a>.<br /> */}
         {/* Based on{' '} */}
         {/* <a href="https://facebook.github.io/create-react-app/"> */}
