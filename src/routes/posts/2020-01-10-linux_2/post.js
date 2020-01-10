140ead7bc4c4f6951edf6bbc48743ec1609de6f9
Mostafa <gmelkabany@gmail.com>
2020-01-10 16:23:53 +0200

    h

---
+++ src/routes/posts/2020-01-10-linux_2/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `How to do git add commit and push in one command`,
+  tags: ['linux', 'shell','git'],
+  spoiler: "using .gitconfig file.",
+  getContent: () => import('./document.mdx'),
+}
